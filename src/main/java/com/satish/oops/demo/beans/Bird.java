dc8c35c79eecce9994c95b214d3cf4bf254ec499
Kumar <skuma596@LAMU02XHM07JG5J.uhc.com>
2019-12-10 00:07:39 +0530

    adding sing behavior to bird

--- src/main/java/com/satish/oops/demo/beans/Bird.java
+++ src/main/java/com/satish/oops/demo/beans/Bird.java
@@ -1,7 +1,21 @@
 package com.satish.oops.demo.beans;
 
+/**
+ * Defines a Bird based on Animal
+ * @see Animal
+ */
 public class Bird extends Animal {
     public void fly() {
         System.out.println("I am flying");
+    }
+
+    /**
+     * Sing the song
+     * @return the song lyrics
+     */
+    public String sing() {
+        String song = "I am singing";
+        System.out.println(song);
+        return song;
     }
 }

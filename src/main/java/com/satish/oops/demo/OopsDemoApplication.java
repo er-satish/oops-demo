1a9217a5ac28fe8c2b1c52c93823cab4774721a7
Kumar <skuma596@LAMU02XHM07JG5J.uhc.com>
2019-12-09 23:35:51 +0530

    initial project setup based on spring boot

---
+++ src/main/java/com/satish/oops/demo/OopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.satish.oops.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satish/oops/demo/OopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satish.oops.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

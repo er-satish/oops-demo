f8a9ad037c2b66d0e1f76697b8eb8a595571c1c9
Kumar <skuma596@LAMU02XHM07JG5J.uhc.com>
2019-12-09 23:43:47 +0530

    adding initial source code

---
+++ src/main/java/com/satish/oops/demo/Solution.java
@@ -0,0 +1,13 @@
+package com.satish.oops.demo;
+
+import com.satish.oops.demo.beans.Bird;
+
+public class Solution {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        //TODO impl. in next PR
+        //bird.sing();
+    }
+}
